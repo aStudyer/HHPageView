ffaead08248f3c3021d010ef66206063c25b6952
aStudyer <astudyer@163.com>
2019-09-14 15:47:34 +0800

    Initial commit

---
+++ Example/HHPageView/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  HHPageView
+//
+//  Created by aStudyer on 09/14/2019.
+//  Copyright (c) 2019 aStudyer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
